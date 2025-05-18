8c1734f282e5ad5412efccc5645d7a327cfacd48
--replace-all <fuad@ceegees.in>
2025-05-18 15:10:35 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/EventReg/',
   plugins: [
     react(),
     VitePWA({
@@ -17,6 +18,7 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
+        start_url: '/EventReg/',
         icons: [
           {
             src: 'pwa-192x192.png',
